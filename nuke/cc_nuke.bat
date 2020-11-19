47de8747273e00a805f6a85cd0dde31a4dd1e327
Akash Srivastava <artistakash.3d@gmail.com>
2020-11-19 16:02:53 +0530

    added bat file lauchers for cece

---
+++ nuke/cc_nuke.bat
@@ -0,0 +1,11 @@
+@echo off
+color A
+
+set SELF_PATH=%~dp0
+set NUKE_VERSION=%1
+set NUKE_APP_VERSION=%2
+set NUKE_PATH=%SELF_PATH%/_setup;
+
+:: Lauch Nuke 
+"%ProgramFiles%/Nuke%NUKE_APP_VERSION%/Nuke%NUKE_VERSION%.exe"
+::pause
